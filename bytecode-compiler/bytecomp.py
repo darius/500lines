bef28322a17d2d9160c9286723d60d63c6af4e12
Darius Bacon <darius@wry.me>
2013-12-12 01:58:36 -0300

    emit bytes compositionally. while loops don't fit well.

--- bytecode-compiler/bytecomp.py
+++ bytecode-compiler/bytecomp.py
@@ -1,19 +1,39 @@
 """
-Byte-compile trivial programs like "print(2+3)".
+Byte-compile trivial programs with if, while, globals, calls, and
+a bit more.
+
+I don't know if this mostly-functional style is the way to go;
+fix_jumps() and especially JUMP_BACK are horrible. That part'd work
+much better if bytecode had only relative jumps, instead of sometimes
+relative sometimes absolute.
+
+So how should we do this? Some possibilities:
+
+  * Emit something like symbolic assembly, with a second pass making
+    actual bytecode. The current scheme is really a variant of this
+    now, with fix_jumps() as the second pass. 
+
+  * Emit code statefully, so you know absolute addresses (fixing up
+    references as you get to them). I expect this to need more lines
+    of code, and to be less declarative but more pythonic.
 """
 
 import ast
 from collections import defaultdict
-from dis import dis, opmap 
+from dis import dis, opmap, HAVE_ARGUMENT
 from types import CodeType, FunctionType
 
 class Opcodes: pass
-opc = Opcodes()
-opc.__dict__.update(opmap)
+op = Opcodes()
+def take_arg(opcode):
+    return lambda arg: bytes((opcode, arg % 256, arg // 256))
+for name, opcode in opmap.items():
+    setattr(op, name,
+            bytes((opcode,)) if opcode < HAVE_ARGUMENT else take_arg(opcode))
+op.JUMP_BACK = take_arg(255)    # A fake opcode for our internal use.
 
 def bytecomp(node, f_globals):
-    code = CodeGen().compile(node)
-    return FunctionType(code, f_globals)
+    return FunctionType(CodeGen().compile(node), f_globals)
 
 class CodeGen(ast.NodeVisitor):
 
@@ -22,11 +42,11 @@ class CodeGen(ast.NodeVisitor):
         self.names     = make_table()
         self.varnames  = make_table()
 
-    def compile(self, node):
-        bytecode = self.visit(node)
-        bytecode = self.sequence([bytecode,
-                                  ((opc.LOAD_CONST,) + encode(self.constants[None])
-                                   + (opc.RETURN_VALUE,))])
+    of = ast.NodeVisitor.visit
+
+    def compile(self, t):
+        bytecode = (self.of(t)
+                    + op.LOAD_CONST(self.constants[None]) + op.RETURN_VALUE)
         argcount = 0
         kwonlyargcount = 0
         nlocals = 0
@@ -37,46 +57,70 @@ class CodeGen(ast.NodeVisitor):
         firstlineno = 1
         lnotab = b''
         return CodeType(argcount, kwonlyargcount, nlocals, stacksize, flags,
-                        bytes(bytecode),
+                        fix_jumps(bytecode),
                         collect(self.constants),
                         collect(self.names),
                         collect(self.varnames),
                         filename, name, firstlineno, lnotab,
                         freevars=(), cellvars=())
 
-    def sequence(self, codes):  # TODO: use join method?
-        result = ()
-        for code in codes:
-            if result: result += (opc.POP_TOP,)
-            result += code
-        return result
-
     def visits(self, nodes):
-        return sum(map(self.visit, nodes), ())
+        return b''.join(map(self.of, nodes))
 
-    def visit_Num(self, node):
-        return (opc.LOAD_CONST,) + encode(self.constants[node.n])
+    def visit_Module(self, t):
+        return self.visits(t.body)
 
-    def visit_Name(self, node):
-        return (opc.LOAD_GLOBAL,) + encode(self.names[node.id])  # XXX LOAD_NAME in general
+    def visit_If(self, t):
+        orelse = self.visits(t.orelse)
+        body = self.visits(t.body) + op.JUMP_FORWARD(len(orelse))
+        return self.of(t.test) + op.POP_JUMP_IF_FALSE(len(body)) + body + orelse
 
-    def visit_BinOp(self, node):
-        binops = {ast.Add: opc.BINARY_ADD}
-        return self.visit(node.left) + self.visit(node.right) + (binops[type(node.op)],)
+    def visit_While(self, t):
+        test, body = self.of(t.test), self.visits(t.body)
+        branch = op.POP_JUMP_IF_FALSE(len(body)+3)
+        inside = test + branch + body
+        loop = inside + op.JUMP_BACK(len(inside))
+        return op.SETUP_LOOP(len(loop)) + loop + op.POP_BLOCK
 
-    def visit_Call(self, node):
-        return (self.visit(node.func) + self.visits(node.args) + (opc.CALL_FUNCTION,) + encode(len(node.args)))
+    def visit_Expr(self, t):
+        return self.of(t.value) + op.POP_TOP
 
-    def visit_Expr(self, node):
-        return self.visit(node.value)
+    def visit_Assign(self, t):
+        assert 1 == len(t.targets) and isinstance(t.targets[0], ast.Name)
+        name = self.names[t.targets[0].id]
+        return self.of(t.value) + op.DUP_TOP + op.STORE_GLOBAL(name)
 
-    def visit_Assign(self, node):
-        assert 1 == len(node.targets)
-        assert isinstance(node.targets[0], ast.Name)
-        return self.visit(node.value) + (opc.DUP_TOP, opc.STORE_GLOBAL,) + encode(self.names[node.targets[0].id])
+    def visit_Call(self, t):
+        return self.of(t.func) + self.visits(t.args) + op.CALL_FUNCTION(len(t.args))
 
-    def visit_Module(self, node):
-        return self.sequence(map(self.visit, node.body))
+    def visit_BinOp(self, t):
+        return self.of(t.left) + self.of(t.right) + self.ops2[type(t.op)]
+    ops2 = {ast.Add:    op.BINARY_ADD,      ast.Sub:      op.BINARY_SUBTRACT,
+            ast.Mult:   op.BINARY_MULTIPLY, ast.Div:      op.BINARY_TRUE_DIVIDE,
+            ast.Mod:    op.BINARY_MODULO,   ast.Pow:      op.BINARY_POWER,
+            ast.LShift: op.BINARY_LSHIFT,   ast.RShift:   op.BINARY_RSHIFT,
+            ast.BitOr:  op.BINARY_OR,       ast.BitXor:   op.BINARY_XOR,
+            ast.BitAnd: op.BINARY_AND,      ast.FloorDiv: op.BINARY_FLOOR_DIVIDE}
+
+    def visit_Num(self, t):
+        return op.LOAD_CONST(self.constants[t.n])
+
+    def visit_Name(self, t):
+        return op.LOAD_GLOBAL(self.names[t.id])  # XXX LOAD_NAME in general
+
+def fix_jumps(bytecode):
+    i, result = 0, list(bytecode)
+    while i < len(bytecode):
+        opcode = bytecode[i]
+        if opcode == opmap['POP_JUMP_IF_FALSE']:
+            target = i + 3 + bytecode[i+1] + 256 * bytecode[i+2]
+            result[i+1], result[i+2] = target % 256, target // 256
+        elif opcode == 255:   # op.JUMP_BACK
+            target = i - (bytecode[i+1] + 256 * bytecode[i+2])
+            result[i] = opmap['JUMP_ABSOLUTE']
+            result[i+1], result[i+2] = target % 256, target // 256
+        i += 1 if opcode < HAVE_ARGUMENT else 3
+    return bytes(result)
 
 def make_table():
     table = defaultdict(lambda: len(table))
@@ -85,17 +129,21 @@ def make_table():
 def collect(table):
     return tuple(sorted(table, key=table.get))
 
-def encode(u):
-    return (u % 256, u // 256)
-
 
 if __name__ == '__main__':
     eg_ast = ast.parse("""
 a = 2+3
-print(a, 137)
+while a:
+    if a - 1:
+        print(a, 137)
+    a = a - 1
 print(pow(2, 16))
-    """)
-    print(ast.dump(eg_ast))
+""")
+    try:
+        import astpp
+    except ImportError:
+        astpp = ast
+    print(astpp.dump(eg_ast))
     f = bytecomp(eg_ast, globals())
     dis(f)
     f()   # It's alive!
